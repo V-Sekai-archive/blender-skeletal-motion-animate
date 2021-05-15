e09b3712e57a0f6d1825253eeebf64beeca60825
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-05-15 14:03:55 -0700

    Missed one.

--- panels/__init__.py
+++ panels/__init__.py
@@ -1,13 +1,11 @@
 if "bpy" not in locals():
     import bpy
     from . import main
-    from . import objects
     from . import retargeting
     from . import info
 else:
     import importlib
 
     importlib.reload(main)
-    importlib.reload(objects)
     importlib.reload(retargeting)
     importlib.reload(info)
