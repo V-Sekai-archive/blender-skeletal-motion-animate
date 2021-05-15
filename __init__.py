ad34b73d9de2ad9f64b22dca5f0d33cc2554bf23
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-05-15 08:29:26 -0700

    Remove unused code.

--- __init__.py
+++ __init__.py
@@ -86,30 +86,9 @@ classes_always_enable = [  # These non-panels will always be loaded, all non-pan
 ]
 
 
-def check_unsupported_blender_versions():
-    # Don't allow Blender versions older than 2.80
-    if bpy.app.version < (2, 80):
-        unregister()
-        sys.tracebacklimit = 0
-        raise ImportError('\n\nBlender versions older than 2.80 are not supported by Rokoko Studio Live. '
-                          '\nPlease use Blender 2.80 or later.'
-                          '\n')
-
-    # Versions 2.80.0 to 2.80.74 are beta versions, stable is 2.80.75
-    if (2, 80, 0) <= bpy.app.version < (2, 80, 75):
-        unregister()
-        sys.tracebacklimit = 0
-        raise ImportError('\n\nYou are still on the beta version of Blender 2.80!'
-                          '\nPlease update to the release version of Blender 2.80.'
-                          '\n')
-
-
 # register and unregister all classes
 def register():
     print("\n### Loading Rokoko Studio Live for Blender...")
-
-    # Check for unsupported Blender versions
-    check_unsupported_blender_versions()
 
     # Register updater and check for Rokoko Studio Live updates
     updater_ops.register(bl_info, beta_branch)
